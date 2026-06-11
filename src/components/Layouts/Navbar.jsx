93c360105438448d089ea69ca3aa78b189d1a4e8
eddylearning <157018938+eddylearning@users.noreply.github.com>
2026-06-11 10:16:16 -0700

    addded login link to navbar list

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -11,6 +11,7 @@ const Navbar = () => {
           <li><a href="/events">Events</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
+          <li><a href="login"></a>Login</li>
         </ul>
       </div>
     </nav>
